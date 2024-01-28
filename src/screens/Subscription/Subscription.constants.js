ab82eeb86e77524758d8e6b665a84b435fc2ee14
Mikel Garcia <mikelgarciaurbina@gmail.com>
2024-01-28 23:05:42 +0700

    Change Plans constant

--- src/screens/Subscription/Subscription.constants.js
+++ src/screens/Subscription/Subscription.constants.js
@@ -1,7 +1,12 @@
 const PLANS = [
-  { id: 'monthly', currency: 'THB', price: 79, caption: '1 month' },
-  { id: 'yearly', currency: 'THB', price: 499, caption: '1 year' },
-  { id: 'lifetime', currency: 'THB', price: 999, caption: 'Lifetime', detail: "Play once and don't worry ever" },
+  { productId: 'monthly', price: '$2.99', title: '1 month' },
+  { productId: 'yearly', price: '$24.99', title: '1 year' },
+  {
+    productId: 'lifetime',
+    price: '$49.99',
+    title: 'Lifetime',
+    description: "Pay once and don't worry ever",
+  },
 ];
 
 export { PLANS };
