e28e056b0d96a8ebbe07ca468fe0d784a9d911b1
soyjavi <hello@soyjavi.com>
2025-01-21 11:42:56 +0700

    <LineChart> improved

--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -4,46 +4,60 @@ import React from 'react';
 import StyleSheet from 'react-native-extended-stylesheet';
 import { CurveType, LineChart as LineChartBase } from 'react-native-gifted-charts';
 
+import { getLastMonths } from './helpers';
 import { style } from './LineChart.style';
 import { useStore } from '../../contexts';
 import { C, L10N } from '../../modules';
-// ! TODO: Should refactor this method
-import { getLastMonths } from '../../screens/Stats/components/SliderMonths/modules';
 import { PriceFriendly } from '../PriceFriendly';
-
-const { COLOR, STATS_MONTHS_LIMIT } = C;
 
 const LineChart = ({
   color: propColor,
   currency,
   height,
+  multipleData = false,
+  pointerConfig = {},
   showPointer = false,
   values = [],
   width,
+  onPointerChange = () => {},
   style: propStyle,
   ...props
 }) => {
-  const { settings: { colorCurrency = false } = {} } = useStore();
+  const { settings: { colorCurrency } = {} } = useStore();
 
-  const color = propColor || (colorCurrency && COLOR[currency]) || StyleSheet.value('$colorAccent');
+  const colorAccent = (colorCurrency && C.COLOR[currency]) || StyleSheet.value('$colorAccent');
 
-  values = values.filter((value) => typeof value === 'number' && !isNaN(value));
-  const max = Math.max(...values);
-  const min = Math.min(...values);
+  const [color, color2] = multipleData ? propColor : [propColor || colorAccent];
+  let [data = [], data2 = []] = multipleData ? values : [values];
+
+  data = data.filter((value) => typeof value === 'number' && !isNaN(value));
+  const max = Math.max(...data);
+  const min = Math.min(...data);
   const range = max - min;
   const maxValue = max + range * 0.05;
   const minValue = min - range * 0.15;
 
-  const months = getLastMonths(STATS_MONTHS_LIMIT);
+  const months = getLastMonths();
 
   return (
     <View style={{ height, opacity: 1, width: '100%', ...propStyle }}>
       <LineChartBase
-        {...{ color, height, maxValue, minValue, width }}
+        {...{
+          color,
+          height,
+          maxValue,
+          minValue,
+          width,
+        }}
         adjustToWidth
         curved
         curveType={CurveType.QUADRATIC}
-        data={values.map((value, index) => ({ index, value, dataPointText: parseInt(value) }))}
+        data={data.map((value, index) => ({ index, value: parseInt(value), dataPointText: parseInt(value) }))}
+        data2={data2?.map((value, index) => ({
+          index,
+          value: parseInt(value),
+          dataPointText: parseInt(value),
+        }))}
         disableScroll
         endSpacing={0}
         hideAxesAndRules
@@ -59,27 +73,35 @@ const LineChart = ({
         startOpacity={0.33}
         endOpacity={0}
         endFillColor={color}
+        {...(multipleData ? { color2, startFillColor2: color2, endFillColor2: color2 } : {})}
         // -- pointer
         pointerConfig={
           showPointer
             ? {
                 autoAdjustPointerLabelPosition: true,
-                initialPointerIndex: values.length ? values.length - 1 : undefined,
-                pointerLabelComponent: ([{ index, value }]) => (
-                  <View align="center">
-                    <Text bold tiny style={style.pointerCaption}>
-                      {L10N.MONTHS[months[index - 1]?.month || 0]} {months[index - 1]?.year || ''}
-                    </Text>
-                    <View style={style.pointerValue}>
-                      <PriceFriendly bold caption color="base" {...{ currency, value }} />
+                initialPointerIndex: data.length ? data.length - 1 : undefined,
+                pointerLabelComponent: ([{ index, value } = {}]) => {
+                  onPointerChange(index);
+
+                  return !multipleData ? (
+                    <View align="center">
+                      <Text bold tiny style={style.pointerCaption}>
+                        {L10N.MONTHS[months[index]?.month || 0]} {months[index]?.year || ''}
+                      </Text>
+                      <View style={style.pointerValue}>
+                        <PriceFriendly bold caption color="base" {...{ currency, value }} />
+                      </View>
                     </View>
-                  </View>
-                ),
+                  ) : null;
+                },
+                pointerVanishDelay: 0,
                 pointerLabelHeight: 48,
                 pointerLabelWidth: 96,
                 pointerColor: color,
+                pointer2Color: color2,
                 pointerStripColor: StyleSheet.value('$colorContentLight'),
                 pointerStripWidth: 1,
+                ...pointerConfig,
               }
             : undefined
         }
@@ -90,13 +112,16 @@ const LineChart = ({
 };
 
 LineChart.propTypes = {
-  color: PropTypes.string,
+  color: PropTypes.any,
   currency: PropTypes.string,
   height: PropTypes.number,
+  multipleData: PropTypes.bool,
+  pointerConfig: PropTypes.shape({}),
   showPointer: PropTypes.bool,
-  values: PropTypes.arrayOf(PropTypes.number),
+  values: PropTypes.any,
   width: PropTypes.number,
   style: PropTypes.any,
+  onPointerChange: PropTypes.func,
 };
 
 export { LineChart };
