684150095bcd1f4450918b6f1ad6b95298a1edf6
soyjavi <hello@soyjavi.com>
2025-02-22 07:58:06 +0700

    Use of <InputDate>

--- src/screens/Clone/Clone.style.jsx
+++ src/screens/Clone/Clone.style.jsx
@@ -2,7 +2,7 @@ import StyleSheet from 'react-native-extended-stylesheet';
 
 export const style = StyleSheet.create({
   title: {
-    marginBottom: '$viewOffset / 2',
+    marginBottom: '$viewOffset',
   },
 
   buttons: {
