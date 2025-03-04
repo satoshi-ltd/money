2b2d25e1417031c1ba5b63be93c140c0ebc0172c
Mikel Garcia <mikelgarciaurbina@gmail.com>
2025-03-04 12:56:18 +0100

    Safearea for Android's notifications.

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,11 +1,15 @@
 import { Notification as NotificationBase } from '@satoshi-ltd/nano-design';
 import React, { useEffect, useState } from 'react';
+import { Platform } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { C, eventEmitter, L10N } from '../../modules';
 
 const { EVENT } = C;
 
 export const Notification = () => {
+  const { top } = useSafeAreaInsets();
+
   const [value, setValue] = useState();
   const [visible, setVisible] = useState(false);
 
@@ -37,6 +41,7 @@ export const Notification = () => {
       {...{ error, text, visible }}
       title={title || (error ? L10N.ERROR : 'Info')}
       onClose={handleClose}
+      style={Platform.OS === 'android' ? { marginTop: top } : undefined}
     />
   );
 };
