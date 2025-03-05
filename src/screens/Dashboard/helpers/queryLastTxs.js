506103a27cdaf9ff02a38c2c607b72d05478a68f
soyjavi <hello@soyjavi.com>
2025-03-05 10:58:59 +0700

    Improved queryLastTxs

--- src/screens/Dashboard/helpers/queryLastTxs.js
+++ src/screens/Dashboard/helpers/queryLastTxs.js
@@ -5,8 +5,8 @@ const { CURRENCY, TRANSACTIONS_PER_PAGE } = C;
 export const queryLastTxs = ({ accounts = [], page = 1, txs = [] }) =>
   groupTxsByDate(
     txs
-      .slice(-(TRANSACTIONS_PER_PAGE * page))
-      .reverse()
+      .sort((a, b) => b.timestamp - a.timestamp)
+      .slice(0, TRANSACTIONS_PER_PAGE * page)
       .map((tx = {}) => {
         const { currency = CURRENCY } = accounts.find(({ hash }) => hash === tx.account) || {};
 
